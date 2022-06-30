2add2d7f6ce119b20d8953ab2a88e63b7fd67e5b
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-06-30 06:06:36 -0300

    instala firebase

---
+++ front-end/src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJgJmm9bog6P2IUghth9IoK3iJvwDkzQU",
+  authDomain: "intranet-cifra-cf7b5.firebaseapp.com",
+  projectId: "intranet-cifra-cf7b5",
+  storageBucket: "intranet-cifra-cf7b5.appspot.com",
+  messagingSenderId: "903350256682",
+  appId: "1:903350256682:web:d92a819358aab6c9782f53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
