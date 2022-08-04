909595da8240036e4fb80efcb22bd37f782be239
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-08-04 14:31:44 -0300

    adiciona componente card.link do react-bootstrap envolta do cardImgStyled

--- front-end/src/components/CardPortals/index.js
+++ front-end/src/components/CardPortals/index.js
@@ -5,11 +5,13 @@ import styled from "styled-components";
 export function CardPortals ({ portal }) {
     return (
         <Card className="text-center shadow h-100">
-            <CardImgStyled variant="top" src={ portal.image } alt={ portal.name } />
+            <Card.Link href={portal.url} target="_blank">
+                <CardImgStyled variant="top" src={ portal.image } alt={ portal.name } />
+            </Card.Link>
             <Card.Body className="d-flex flex-column align-items-center">
                 <Card.Title as='h2' className='h5'>{portal.name}</Card.Title>
                 <Card.Text>{portal.shortDescription}</Card.Text>
-                <Button as={Link} to={`/portals/${portal.id}`} className='mt-auto'>Acessar</Button>
+                <Button as={Link} to={`/portals/${portal.id}`} className='mt-auto'>Sobre</Button>
             </Card.Body>
         </Card>
     )
