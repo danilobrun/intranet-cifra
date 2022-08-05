a6c6ad44e94021cd8eff5b4d36c66cfe69299b24
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-08-05 15:29:28 -0300

    fet: atualiza footer estiliza com styled component e alinha ao centro

--- front-end/src/components/Layout/Footer.js
+++ front-end/src/components/Layout/Footer.js
@@ -1,7 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
 export default function Footer () {
-    return (
-      <footer className='bg-secondary bg-gradient text-white text-center py-2 mt-4 footer'>
-        <p className='m-0'>Todos os direitos reservados a Cifra Engenharia.</p>
-      </footer>
-    )
-  }
+  return (
+    <FooterStyled className="bg-secondary bg-gradient text-center py-2 mt-4">
+    <Container className="d-lg-flex align-items-center justify-content-center">
+        <Nav className="flex-column flex-lg-row my-4 my-lg-0">
+            <Nav.Link as={Link} to='/' className="text-white">Início</Nav.Link>
+            <Nav.Link as={Link} to='/portal/login' className="text-white">Cadastro</Nav.Link>
+            <Nav.Link as={Link} to='/portal/login' className="text-white">Login</Nav.Link>
+            <Nav.Link href="/termos-de-uso.pdf" target="_blank" className="text-white">Termos de Uso</Nav.Link>
+        </Nav>
+        <Nav className="justify-content-center">
+            <Nav.Link href="https://www.linkedin.com/company/cifra-engenharia" target="_blank" className="text-white">
+                <IconStyled icon={faLinkedin} />
+            </Nav.Link>
+            <Nav.Link href="https://www.instagram.com" target="_blank" className="text-white">
+                <IconStyled icon={faInstagram} />
+            </Nav.Link>
+        </Nav>
+    </Container>
+</FooterStyled>
+  )
+}
+
+const FooterStyled = styled.footer`
+    padding: 35px 0 40px;
+    @media (min-width: 992px) {
+        padding: 15px 0;
+    }
+`
+
+const IconStyled = styled(FontAwesomeIcon)`
+    font-size: 40px;
+`
