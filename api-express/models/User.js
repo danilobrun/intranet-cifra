31f3271cde98247bf7f775d088af02f32a62d4cd
Danilo Brun <danilosbrun@gmail.com>
2022-11-10 21:39:18 -0300

    chore: atualiza user model adicionando type

--- api-express/models/User.js
+++ api-express/models/User.js
@@ -1,14 +1,15 @@
 // IMPORTS
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // Acesssos e propriedades dessa classe
-const User = mongoose.model('User', {
-    name: String,
-    email: String,
-    password: String,
-    createdAt: Date,
-    updatedAt: Date
-})
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  password: String,
+  type: Number,
+  createdAt: Date,
+  updatedAt: Date,
+});
 
 // EXPORTS
-module.exports = User
+module.exports = User;
