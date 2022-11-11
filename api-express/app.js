dae83f17b60b6f919625ad338636ade927a71ca4
Danilo Brun <danilosbrun@gmail.com>
2022-11-10 21:37:14 -0300

    chore: atualiza porta da API e instala cors para liberar requisições.

--- api-express/app.js
+++ api-express/app.js
@@ -1,28 +1,30 @@
 /* IMPORTS */
 require("dotenv").config();
+var cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const registersRoutes = require("./src/routes")
+const registersRoutes = require("./src/routes");
 
+const app = express();
 
-const app = express()
+app.use(cors());
 
 // Config JSON response middleware
-app.use(express.json())
+app.use(express.json());
 
-registersRoutes(app)
+registersRoutes(app);
 
 // Credencials
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
 // Connect method Mongo DB
 mongoose
-    .connect(
-        `mongodb+srv://${dbUser}:${dbPassword}@cluster0.zti1m9u.mongodb.net/?retryWrites=true&w=majority`
-        )
-    .then(() => {
-        app.listen(3002)
-        console.log('Conectou ao banco!')
-    })
-    .catch((err) => console.log(err))
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.zti1m9u.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3002);
+    console.log("Conectou ao banco!");
+  })
+  .catch((err) => console.log(err));
