4032ad83fb1999477176b28addfa5b37d3960e5b
Danilo Brun <danilosbrun@gmail.com>
2022-11-16 14:18:26 -0300

    front: ajusta serviço do portal ajusta rota para criar portal

--- front-end/src/services/Portals.service.js
+++ front-end/src/services/Portals.service.js
@@ -1,57 +1,59 @@
-import { apiUrl, getAuthorizationHeaders } from "./Api.service"
+import { apiUrl, getAuthorizationHeaders } from "./Api.service";
 
 export const getPortals = async () => {
-    const response = await fetch(`${apiUrl}/portals`)
-            if (!response.ok) {
-                throw new Error('Reponse not ok.')
-            }
-            return response.json()
-}
+  const response = await fetch(`${apiUrl}/portals`);
+  if (!response.ok) {
+    throw new Error("Reponse not ok.");
+  }
+  return response.json();
+};
 
 export const getPortalById = async (portalId) => {
-    const response = await fetch(`${apiUrl}/portals/${portalId}?_embed=inscriptions`)
-            if (!response.ok) {
-                throw new Error('Reponse not ok.')
-            }
-            return response.json()
-}
+  const response = await fetch(
+    `${apiUrl}/portals/${portalId}?_embed=inscriptions`
+  );
+  if (!response.ok) {
+    throw new Error("Reponse not ok.");
+  }
+  return response.json();
+};
 
-export const deletePortal = async portalId => {
-    const response = await fetch(`${apiUrl}/portals/${portalId}`, {
-        method: 'DELETE',
-        headers: getAuthorizationHeaders()
-    })
-    if (!response.ok) {
-        throw new Error('Reponse not ok.')
-    }
-}
+export const deletePortal = async (portalId) => {
+  const response = await fetch(`${apiUrl}/portals/${portalId}`, {
+    method: "DELETE",
+    headers: getAuthorizationHeaders(),
+  });
+  if (!response.ok) {
+    throw new Error("Reponse not ok.");
+  }
+};
 
 export const createPortal = async (portalData) => {
-    const body = JSON.stringify(portalData)
-    const response = await fetch(`${apiUrl}/portals`, {
-        method: 'POST',
-        body,
-        headers: {
-            'content-type': 'application/json',
-            ...getAuthorizationHeaders()
-        }
-    })
-    if (!response.ok) {
-        throw new Error('Response not ok.') 
-    }
-}
+  const body = JSON.stringify(portalData);
+  const response = await fetch(`${apiUrl}/portals/auth/register`, {
+    method: "POST",
+    body,
+    headers: {
+      "content-type": "application/json",
+      ...getAuthorizationHeaders(),
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Response not ok.");
+  }
+};
 
 export const updatePortal = async (portalId, portalData) => {
-    const body = JSON.stringify(portalData)
-    const response = await fetch(`${apiUrl}/portals/${portalId}`, {
-        method: 'PUT',
-        body,
-        headers: {
-            'content-type': 'application/json',
-            ...getAuthorizationHeaders()
-        }
-    })
-    if (!response.ok) {
-        throw new Error('Response not ok.')
-    }
-}
+  const body = JSON.stringify(portalData);
+  const response = await fetch(`${apiUrl}/portals/${portalId}`, {
+    method: "PUT",
+    body,
+    headers: {
+      "content-type": "application/json",
+      ...getAuthorizationHeaders(),
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Response not ok.");
+  }
+};
