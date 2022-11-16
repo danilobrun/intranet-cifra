60e6496116f2ca2065c02c88460ae4b94f08ea09
Danilo Brun <danilosbrun@gmail.com>
2022-11-16 14:19:06 -0300

    front: ajusta tora do createInscriptions

--- front-end/src/services/Inscriptions.service.js
+++ front-end/src/services/Inscriptions.service.js
@@ -1,16 +1,16 @@
-import { apiUrl, getAuthorizationHeaders } from "./Api.service"
+import { apiUrl, getAuthorizationHeaders } from "./Api.service";
 
 /*Requisição de inscrição tem que passar o header com a chave accessToken do usuário*/
 export const createInscriptions = async (inscriptionData) => {
-    const response = await fetch(`${apiUrl}/inscriptions`, {
-                method: 'POST',
-                body: JSON.stringify(inscriptionData), /*convertido em JSON string*/
-                headers: {
-                    'content-type': 'application/json',
-                    ...getAuthorizationHeaders() /*função específica para trazer o accessToken do usuário*/
-                }
-            })
-            if (!response.ok) {
-                throw new Error('Reponse not ok.')
-            }
-}
+  const response = await fetch(`${apiUrl}/inscriptions/auth/register`, {
+    method: "POST",
+    body: JSON.stringify(inscriptionData) /*convertido em JSON string*/,
+    headers: {
+      "content-type": "application/json",
+      ...getAuthorizationHeaders() /*função específica para trazer o accessToken do usuário*/,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Reponse not ok.");
+  }
+};
