34c2b62eeaacfa96e512dc83ae95dea0ab70b91a
Danilo Brun <danilosbrun@gmail.com>
2022-11-23 10:27:10 -0300

    feat: adicionar propriedade url em upsertportalfrom

--- front-end/src/views/AdminEditPortal/index.js
+++ front-end/src/views/AdminEditPortal/index.js
@@ -6,48 +6,49 @@ import { Loading } from "../../components/Loading";
 import { UpsertPortalForm } from "../../components/UpsertPortalForm";
 import { getPortalById, updatePortal } from "../../services/Portals.service";
 
-export function AdminEditPortalView () {
-    const { id } = useParams()
-    const [portal, setPortal] = useState()
-    useEffect(() => {
-        const fetchPortal = async () => {
-            try {
-                const portalData = await getPortalById(id)
-                setPortal(portalData);
-            } catch {
-                toast.error('Falha ao buscar dados do portal. Recarregue a página.')
-            }
-        }
-        fetchPortal()
-    }, [id])
-    const navigate = useNavigate()
-    const handleSubmit = async (formData) => {
-        try {
-            await updatePortal(id, formData)
-            toast.success('Portal alterado com sucesso.')
-            navigate('/portal/portals')
-        } catch {
-            toast.error('Falha ao alterar portal. Tente novamente.')
-        }
+export function AdminEditPortalView() {
+  const { id } = useParams();
+  const [portal, setPortal] = useState();
+  useEffect(() => {
+    const fetchPortal = async () => {
+      try {
+        const portalData = await getPortalById(id);
+        setPortal(portalData);
+      } catch {
+        toast.error("Falha ao buscar dados do portal. Recarregue a página.");
+      }
+    };
+    fetchPortal();
+  }, [id]);
+  const navigate = useNavigate();
+  const handleSubmit = async (formData) => {
+    try {
+      await updatePortal(id, formData);
+      toast.success("Portal alterado com sucesso.");
+      navigate("/portal/portals");
+    } catch {
+      toast.error("Falha ao alterar portal. Tente novamente.");
     }
-    return (
-        <LayoutPortal>
-            <h1 className="mt-4">Editar portal</h1>
-            {portal ? (
-                <UpsertPortalForm
-                    initialValue={{
-                        name: portal.name,
-                        responsible: portal.responsible,
-                        shortDescription: portal.shortDescription,
-                        description: portal.description,
-                        image: portal.image
-                    }}
-                    buttonLabel='Alterar'
-                    onSubmit={handleSubmit} 
-                />
-            ) : (
-                <Loading />
-            )}
-        </LayoutPortal>
-    )
+  };
+  return (
+    <LayoutPortal>
+      <h1 className="mt-4">Editar portal</h1>
+      {portal ? (
+        <UpsertPortalForm
+          initialValue={{
+            name: portal.name,
+            responsible: portal.responsible,
+            shortDescription: portal.shortDescription,
+            description: portal.description,
+            image: portal.image,
+            url: portal.url,
+          }}
+          buttonLabel="Alterar"
+          onSubmit={handleSubmit}
+        />
+      ) : (
+        <Loading />
+      )}
+    </LayoutPortal>
+  );
 }
