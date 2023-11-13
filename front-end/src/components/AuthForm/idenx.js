97c56c241c50abb587aa9cdd3e3afd540be95ec9
Danilo Brun <danilosbrun@gmail.com>
2023-11-13 14:35:22 -0300

    feat: "atualiza a página de login adicionando header e footer"

--- front-end/src/components/AuthForm/idenx.js
+++ front-end/src/components/AuthForm/idenx.js
@@ -2,41 +2,46 @@ import { Col, Row } from "react-bootstrap";
 import styled from "styled-components";
 import { LoginForm } from "./LoginForm";
 import { RegisterForm } from "./RegisterForm";
+import { Layout } from "../Layout";
 
-export function AuthForm ({ redirectAfterLogin = true }) {
-    return (
-        <Row>
-            <Col xs={12} lg='auto' className="flex-fill">
-                <LoginForm redirectAfterLogin={redirectAfterLogin} />
-            </Col>
-            <Divider xs={12} lg='auto'>
-                ou
-            </Divider>
-            <Col xs={12} lg='auto' className="flex-fill">
-                <RegisterForm redirectAfterLogin={redirectAfterLogin} />
-            </Col>
-        </Row>
-    )
+export function AuthForm({ redirectAfterLogin = true }) {
+  return (
+    <Layout>
+      <Row className="mt-4">
+        <Col xs={12} lg="auto" className="flex-fill">
+          <LoginForm redirectAfterLogin={redirectAfterLogin} />
+        </Col>
+        <Divider xs={12} lg="auto">
+          ou
+        </Divider>
+        <Col xs={12} lg="auto" className="flex-fill">
+          <RegisterForm redirectAfterLogin={redirectAfterLogin} />
+        </Col>
+      </Row>
+    </Layout>
+  );
 }
 
 const Divider = styled(Col)`
-    text-align: center;
-    padding: 16px 0;
-    ::before, ::after {
-        content: '';
-        width: 30px;
-        height: 1px;
-        background-color: #999;
-        display: inline-block;
-        vertical-align: middle;
-        margin: 0 8px;
+  text-align: center;
+  padding: 16px 0;
+  ::before,
+  ::after {
+    content: "";
+    width: 30px;
+    height: 1px;
+    background-color: #999;
+    display: inline-block;
+    vertical-align: middle;
+    margin: 0 8px;
+  }
+  @media (min-width: 992px) {
+    padding-left: 42px;
+    padding-right: 42px;
+    align-self: center;
+    ::before,
+    ::after {
+      display: none;
     }
-    @media (min-width: 992px) {
-        padding-left: 42px;
-        padding-right: 42px;
-        align-self: center;
-        ::before, ::after {
-            display: none;
-        }
-    }
-`
+  }
+`;
