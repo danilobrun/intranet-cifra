a12111571bed3664d8333258999b5c1d49cf0eb3
Danilo Humberto <danilohumberto12@hotmail.com>
2026-04-06 08:38:22 -0300

    feat: add authentication middleware to user routes for enhanced security

--- api-express/src/routes/users.routes.js
+++ api-express/src/routes/users.routes.js
@@ -12,14 +12,14 @@ const {
 } = require("../presentation/controllers/users.controller");
 
 const usersRoutes = (app) => {
-  app.get("/users", listUsers);
+  app.get("/users", checkToken, listUsers);
   app.get("/user/:id", checkToken, getUserById);
   app.post("/auth/register", createUser);
   app.post("/auth/login/", loginUser);
   app.post("/auth/forgot-password/request", requestPasswordResetCode);
   app.post("/auth/forgot-password/verify", verifyPasswordResetCode);
   app.post("/auth/forgot-password/reset", resetUserPassword);
-  app.delete("/user/:id", deleteUser);
+  app.delete("/user/:id", checkToken, deleteUser);
   app.put("/user/:id", checkToken, editUser);
 };
 
