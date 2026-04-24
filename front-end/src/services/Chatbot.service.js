67b723f7817682eedeb9d8d19810c2d011d9b05a
Danilo Humberto <danilohumberto12@hotmail.com>
2026-04-24 10:28:37 -0300

    fix: update chatbot URL in Chatbot.service.js

--- front-end/src/services/Chatbot.service.js
+++ front-end/src/services/Chatbot.service.js
@@ -1,5 +1,5 @@
 const chatbotUrl =
-  "https://powder-contributing-eliminate-codes.trycloudflare.com/webhook/chatbot-frota";
+  "https://careers-supposed-favor-separately.trycloudflare.com/webhook/chatbot-frota";
 
 const parseResponseBody = async (response) => {
   const rawBody = await response.text();
